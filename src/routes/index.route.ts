bf37ee9a018ad48b87f104ea813fdc8117763979
facundoacostayl <facundoacostayl@outlook.com>
2023-07-13 07:50:28 -0300

    dynamic routes with fs

---
+++ src/routes/index.route.ts
@@ -0,0 +1,18 @@
+import fs from "fs";
+import Router from "express";
+
+const router = Router();
+const currentPath = `${__dirname}`;
+
+const formatFileName = (file: string) => {
+  return file.split(".").shift();
+};
+
+fs.readdirSync(currentPath).filter((file) => {
+  const cleanFile = formatFileName(file);
+  if (cleanFile != "index") {
+    import(`./${cleanFile}.route`).then((route) => {
+      router.use(`/${cleanFile}`, route.router);
+    });
+  }
+});
