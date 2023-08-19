6d8f890bbdd7af8f8aef4144dbf80cc2b545e7d7
facundoacostayl <facundoacostayl@outlook.com>
2023-08-19 05:14:55 -0300

    Add unit tests

---
+++ src/__tests__/goal.tests.ts
@@ -0,0 +1,9 @@
+describe("goal", () => {
+  describe("get a goal", () => {
+    describe("given goal is not found", () => {
+      it("should return true", () => {
+        expect(true).toBe(true);
+      });
+    });
+  });
+});
